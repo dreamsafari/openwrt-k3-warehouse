469df671d292d07868c99642976fcd8470103c97
dreamsafari <mac.liang@live.com>
2020-03-05 14:29:13 +0000

    new file:   upgrade.sh

---
+++ upgrade.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd dreamsafari
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-flowoffload
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/openwrt-fullconenat
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-vlmcsd
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/vlmcsd
+cd /network/config/firewall
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/network/config/firewall/patches/
+cd ../../..
+git submodule foreach git pull
